0de722b53fd6908a3e5c8c60ea940a90df15cf12
Avinash Gupta <avinash.gupta@vils.ai>
2023-05-22 18:55:54 +0530

    bin issue

--- bin/cli.js
+++ bin/cli.js
@@ -17,6 +17,7 @@ const repo_name = process.argv[2];
 const gitCloneCommand = `git clone --depth 1 https://github.com/tier3guy/create-react-app-starter-template ${repo_name}`;
 const npmInstallCommands = `cd ${repo_name} && npm install`;
 const removingGit = `rm -rf .git && git init`;
+const removeBin = "rm -rf bin";
 
 console.log("\n>> Creating new application ...");
 const clonned = runCommand(gitCloneCommand);
@@ -27,6 +28,8 @@ const installed = runCommand(npmInstallCommands);
 if (!installed) process.exit(1);
 const removed = runCommand(removingGit);
 if (!removed) process.exit(1);
+const removedBin = runCommand(removeBin);
+if (!removedBin) process.exit(1);
 
 console.log("\n\n>> Congratulations, you project is ready !");
 console.log(
