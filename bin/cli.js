de33ceda6027ff24498abf648d0355304bce3798
Avinash Gupta <avinash.gupta@vils.ai>
2023-05-22 19:19:09 +0530

    Package

--- bin/cli.js
+++ bin/cli.js
@@ -21,8 +21,7 @@ const gitCloneCommand = `git clone --depth 1 https://github.com/tier3guy/create-
 const npmInstallCommands = `cd ${
   repo_name ? repo_name : "create-react-app-starter-template"
 } && npm install`;
-const removingGit = `rm -rf .git && git init`;
-const removeBin = "rm -rf bin";
+// const removingGit = `git remote rm origin`;
 
 console.log("\n>> Creating new application ...");
 const clonned = runCommand(gitCloneCommand);
@@ -32,13 +31,12 @@ console.log("\n\n>> Installing dependencies to the project ...");
 const installed = runCommand(npmInstallCommands);
 if (!installed) process.exit(1);
 
-const removed = runCommand(removingGit);
-if (!removed) process.exit(1);
-
-const removedBin = runCommand(removeBin);
-if (!removedBin) process.exit(1);
+// const removed = runCommand(removingGit);
+// if (!removed) process.exit(1);
 
 console.log("\n\n>> Congratulations, you project is ready !");
 console.log(
-  `>> run cd ${repo_name} to go to the project directory and npm run start to start the project`
+  `>> run cd ${
+    repo_name ? repo_name : "create-react-app-starter-template"
+  } to go to the project directory and npm run start to start the project`
 );
