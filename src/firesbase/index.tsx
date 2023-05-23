f23ad2865de4a27beada9c8e466f690186c07713
Avinash Gupta <avinash.gupta@vils.ai>
2023-05-23 17:58:13 +0530

    Firebase Initialized

---
+++ src/firesbase/index.tsx
@@ -0,0 +1,7 @@
+// External Imports
+import { initializeApp } from 'firebase/app';
+
+// Firebase Configs
+import firebaseConfig from './config';
+
+export const app = initializeApp(firebaseConfig);
