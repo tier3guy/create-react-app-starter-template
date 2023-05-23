97eeeb162fb8b9e76dce13cb16dbc20ebd199e67
Avinash Gupta <avinash.gupta@vils.ai>
2023-05-23 12:12:59 +0530

    tailwind

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,7 @@
 const Error = () => {
     return (
         <div className="bg-slate-800 h-screen w-screen grid place-content-center">
-            <h1>Error Page</h1>
+            <h1 className="text-2xl text-white">Error Page</h1>
         </div>
     );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <div className="bg-slate-800 h-screen w-screen grid place-content-center">
-            <h1>
+            <h1 className="text-2xl text-white">
                 Hi, welcome to avinash templates / react / typescript / tailwind
             </h1>
         </div>
